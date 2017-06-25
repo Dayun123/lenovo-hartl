dd1b04e131011a1a6bb598f4b1dfe93c189dabdf
Daniel Schroeder <DanielSchroederMusic@gmail.com>
2017-06-24 23:58:53 -0500

    Add an email index to the users table.
    Add validation for uniqueness of email in users model.
    Add test that uniqueness validation of email in users model is working.
    Comment out default values in test/fixtures/users.yml since they were making the uniqueness DB constraint fail.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,7 @@
 class User < ApplicationRecord
+  # Because some database adapters are case sensitive, we use the before_save callback to make sure all emails coming in to this model are downcased before being saved to the database.
+  before_save { self.email.downcase! }
   validates :name, presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
-  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }
+  validates :email, presence: true, length: { maximum: 255 }, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
 end
--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -50,4 +50,19 @@ class UserTest < ActiveSupport::TestCase
     end
   end
 
+  test "email addresses should be unique" do
+    duplicate_user = @user.dup
+    # should be case-insensitive, so we store the duplicate_users email as the upcase version of the original email to see if the validation recognizes that these are the same email.
+    duplicate_user.email = @user.email.upcase
+    @user.save
+    assert_not duplicate_user.valid?
+  end
+
+  test "email addresses should be saved as lower-case" do
+    mixed_case_email = "FooBaR@EXample.COM"
+    @user.email = mixed_case_email
+    @user.save
+    assert_equal mixed_case_email.downcase, @user.reload.email
+  end
+
 end
